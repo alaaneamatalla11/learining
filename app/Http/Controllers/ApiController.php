a841a2a37b001c58a9399d684272d16b6399da5a
alaa <alaaneamatalla@gmail.com>
2023-09-10 14:38:18 +0300

    api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{ 
+    public function index()
+    {
+
+    $products = Product::get();
+    return response()->json([
+"products" => $products
+]);
+    }
+public function store ( Request $request){
+$data = $request->validate([
+"name"=>"required|string",
+"price"=>"required|numeric",
+
+]);
+$products = Product::create($data);
+return response()->json([
+"message" => "product has been added"
+]);
+}
+public function update(Request $request, $id){
+    $data =$request->validate([
+        "price"=>"required|numeric",
+        "name"=>"required|string"
+    ]);
+    $product=Product::findorFail($id)
+    ->update($data);
+    return response()->json([
+        "message"=>"product has been updated"
+    ]);
+}
+
+public function delete($id)
+{
+    $product = Product::findOrFail($id)->delete();
+    return response()->json([
+        "message" => "product has been deleted"
+    ]);
+}}
+
